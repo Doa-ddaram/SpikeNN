bbaa1c5a759831338d72a3c0128ad252f61d1990
doa-ddaram <sqoml123@naver.com>
2025-10-25 10:53:35 +0000

    Update generate_file and requirement(add torchvision)

--- generate_cifar10.py
+++ generate_cifar10.py
@@ -21,6 +21,9 @@ y_train = np.array([label for _, label in cifar_train])        # (50000,)
 X_test = np.stack([np.array(img) for img, _ in cifar_test])  # (10000, 32, 32, 3)
 y_test = np.array([label for _, label in cifar_test])        # (10000,)
 
+from sklearn.model_selection import train_test_split
+X_train, X_val, y_train, y_val = train_test_split(X_train, y_train, test_size=0.1, stratify=y_train, random_state=42)
+
 # (N, H, W, C) = (50000, 32, 32, 3)
 np.save("input/X_train.npy", X_train)
 np.save("input/y_train.npy", y_train)
@@ -29,9 +32,16 @@ np.save("input/y_train.npy", y_train)
 np.save("input/X_test.npy", X_test)
 np.save("input/y_test.npy", y_test)
 
+np.save("input/X_val.npy", X_val)
+np.save("input/y_val.npy", y_val)
+
 print("Complete Save: input/X_train.npy, input/y_train.npy")
 print("X_train shape:", X_train.shape)
 print("y_train shape:", y_train.shape)
+
+print("Complete Save: input/X_val.npy, input/y_val.npy")
+print("X_val shape:", X_val.shape)
+print("y_val shape:", y_val.shape)
 
 print("Complete Save: input/X_test.npy, input/y_test.npy")
 print("X_test shape:", X_test.shape)
